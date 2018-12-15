6578feadef6543cff07a35b23ceb396f99ce4ce2
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2018-12-15 18:57:38 +0200

    Create repository structure

---
+++ metafiles/base.mk
@@ -0,0 +1,28 @@
+HIGHLIGHT = tex/code
+PDF = $(addsuffix .pdf, $(BASENAME))
+TEX = $(addsuffix .tex, $(BASENAME))
+PDFLATEX = pdflatex
+STY_DIR = ../../sty
+OUT_DIR = texfiles
+CODE_DIR = code
+IMG_DIR = img
+
+.PHONY: clean all $(PDF)
+
+all: $(PDF)
+
+$(PDF): $(TEX)
+	# Rebuild source files, if any
+	-test -d $(CODE_DIR) && make -C $(CODE_DIR)
+	-test -d $(IMG_DIR) && make -C $(IMG_DIR)
+	# Create out directory
+	-test -d $(OUT_DIR) || mkdir $(OUT_DIR)
+	# Twice, so TOC is also updated
+	TEXINPUTS=$(STY_DIR)//: $(PDFLATEX) -output-directory $(OUT_DIR) $<
+	TEXINPUTS=$(STY_DIR)//: $(PDFLATEX) -output-directory $(OUT_DIR) $<
+	cp $(OUT_DIR)/$(PDF) .
+
+clean:
+	-rm -fr $(OUT_DIR)
+	-test -d $(CODE_DIR) && make -C $(CODE_DIR) clean
+	-test -d $(IMG_DIR) && make -C $(IMG_DIR) clean
