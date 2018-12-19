c0fa51d57d9330633bf8382d2152eee20ee59900
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2018-12-19 17:33:23 +0200

    070-intregi: Use ^ instead of * when printing exponention

--- 07-intregi/skel/integer-types/prog.c
+++ 07-intregi/skel/integer-types/prog.c
@@ -13,8 +13,8 @@ int main(void)
 	printf("uc: %hhu (0x%02hhx), c: %hhd (0x%02hhx)\n", uc, uc, c, c);
 	printf("ui: %u (0x%08x), i: %u (0x%08x)\n", ui, ui, i, i);
 	printf("%hhu, %hhd, %hu, %hd, %u, %d\n", a, a, a, a, a, a);
-	printf("2*31: %u\n", 1U << 31);
-	printf("2*63: %lu\n", 1UL << 63);
+	printf("2^31: %u\n", 1U << 31);
+	printf("2^63: %lu\n", 1UL << 63);
 	printf("sizeof(1): %zu, sizeof(1U): %zu, sizeof(1L): %zu, sizeof(1UL): %zu, sizeof(1LL): %zu, sizeof(1ULL): %zu\n",
 		sizeof(1), sizeof(1U), sizeof(1L), sizeof(1UL), sizeof(1LL), sizeof(1ULL));
 
